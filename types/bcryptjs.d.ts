6738c99e17ca970243b303d32816bf5509a9ef35
kingfayd <princeboateng2235@gmail.com>
2026-01-31 08:13:06 +0700

    update tsconfig

--- types/bcryptjs.d.ts
+++ types/bcryptjs.d.ts
@@ -1,9 +1,8 @@
-declare module "bcryptjs" {
-  export function hash(data: string, saltOrRounds: number): Promise<string>;
-  export function compare(data: string, encrypted: string): Promise<boolean>;
-  const bcrypt: {
-    hash: typeof hash;
-    compare: typeof compare;
-  };
-  export default bcrypt;
+declare module 'bcryptjs' {
+  export function hash(s: string, salt: number | string): Promise<string>;
+  export function hashSync(s: string, salt: number | string): string;
+  export function compare(s: string, hash: string): Promise<boolean>;
+  export function compareSync(s: string, hash: string): boolean;
+  export function genSalt(rounds?: number): Promise<string>;
+  export function genSaltSync(rounds?: number): string;
 }
